2136f01be06a7bb96816e16329be61b2debe5ece
Greedrache <you@example.com>
2026-01-05 18:05:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StartSectionComponent } from './start-section/start-section.component';
+import { HeaderComponent } from './header/header.component';
+import { WhyMeSectionComponent } from './why-me-section/why-me-section.component';
+import { SkillsSectionComponent } from './skills-section/skills-section.component';
+import { MyProjectsSectionComponent } from './my-projects-section/my-projects-section.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactMeSectionComponent } from './contact-me-section/contact-me-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    StartSectionComponent,
+    WhyMeSectionComponent,
+    SkillsSectionComponent,
+    MyProjectsSectionComponent,
+    FooterComponent,
+    ContactMeSectionComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
