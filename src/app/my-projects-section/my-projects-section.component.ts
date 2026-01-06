367fe5e985aa1297624e037bc282df0e0a3f6715
Greedrache <you@example.com>
2026-01-06 21:26:17 +0100

    verbesserungen der projekt seciton sowie der skills section

--- src/app/my-projects-section/my-projects-section.component.ts
+++ src/app/my-projects-section/my-projects-section.component.ts
@@ -16,26 +16,43 @@ export class MyProjectsSectionComponent {
     {
       title: '1. Join',
       duration: '2 Months',
-      tech: 'JavaScript, HTML, CSS, Firebase, Git',
-      description: 'Join is a handy web app that lets you sign up and create tasks. You can manage your tasks by "To Do," "In Progress," and "Done," set priorities, and enter deadlines. You can also add contacts to stay organized together.',
+      tech: [
+        { name: 'HTML', img: 'assets/img/html.png' },
+        { name: 'CSS', img: 'assets/img/css.png' },
+        { name: 'JavaScript', img: 'assets/img/javascript.png' },
+        { name: 'Firebase', img: 'assets/img/firebase.png' },
+        { name: 'Git', img: 'assets/img/git.png' }
+      ],
+      description: 'Join is a web app that lets you sign up and create tasks. You can manage your tasks by "To Do," "In Progress," and "Done," set priorities, and enter deadlines. You can also add contacts to stay organized together.',
       githubLink: '',
-      img: 'assets/img/join.png'
+      img: 'assets/img/join.jpg'
     },
     {
       title: '2. El Pollo Loco',
       duration: '2 Weeks',
-      tech: 'JavaScript, HTML, CSS, Git',
+      tech: [
+        { name: 'HTML', img: 'assets/img/html.png' },
+        { name: 'CSS', img: 'assets/img/css.png' },
+        { name: 'JavaScript', img: 'assets/img/javascript.png' },
+        { name: 'Git', img: 'assets/img/git.png' }
+      ],
       description: 'El Pollo Loco is a fun jump-and-run game where you jump on chickens and overcome obstacles. At the end of each round, a giant boss chicken awaits that you must defeat. Speed, timing, and a little strategy are the keys to victory.',
       githubLink: 'https://github.com/Greedrache/EL-POLLO-LOCO',
-      img: 'assets/img/elpolloloco.png'
+      img: 'assets/img/elpolloloco.jpg'
     },
     {
       title: '3. Pokedex',
       duration: '2 Weeks',
-      tech: 'JavaScript, HTML, CSS, Git, Firebase',
+      tech: [
+        { name: 'HTML', img: 'assets/img/html.png' },
+        { name: 'CSS', img: 'assets/img/css.png' },
+        { name: 'JavaScript', img: 'assets/img/javascript.png' },
+        { name: 'Git', img: 'assets/img/git.png' },
+        { name: 'Firebase', img: 'assets/img/firebase.png' }
+      ],
       description: 'Pokedex is an interactive website that introduces all Pokémon and loads their information via an API. Here you can discover the characteristics, types, and abilities of each Pokémon. Perfect for expanding your Pokémon knowledge and learning new things about the world of pocket monsters.',
       githubLink: 'https://github.com/Greedrache/Pokedex',
-      img: 'assets/img/pokedex.png'
+      img: 'assets/img/pokedex.jpg'
     }
   ];
 }
