4f76766cae74cce56ebc5889f0cbfcdf18bfa70c
Greedrache <you@example.com>
2026-01-07 12:20:23 +0100

    Start section responsive

--- src/app/start-section/start-section.component.ts
+++ src/app/start-section/start-section.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-start-section',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './start-section.component.html',
   styleUrl: './start-section.component.scss'
 })
 export class StartSectionComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
 }
