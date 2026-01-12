2a6c1e0980ccbcca486e32159d0a8baad992b98f
Greedrache <you@example.com>
2026-01-12 15:56:55 +0100

    Hintergrund und Skills kolledieren nicht mehr. Impressum startet oben

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslationService } from '../services/translation.service';
 import { HeaderComponent } from '../header/header.component';
@@ -9,10 +10,19 @@ import { FooterComponent } from '../footer/footer.component';
   standalone: true,
   imports: [RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './legal-notice.component.html',
-  styleUrl: './legal-notice.component.scss'
+  styleUrls: ['./legal-notice.component.scss']
 })
-export class LegalNoticeComponent {
-  constructor(public translationService: TranslationService) {}
+export class LegalNoticeComponent implements OnInit {
+  constructor(
+    public translationService: TranslationService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    }
+  }
 
   t(key: string): string {
     return this.translationService.translate(key);
