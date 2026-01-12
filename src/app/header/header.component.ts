acc7b04c68bd17f149a0d84d770efc4bc082d1f0
Greedrache <you@example.com>
2026-01-12 22:25:22 +0100

    Kein Refresh der Seite mehr

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 import { TranslationService, Language } from '../services/translation.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   currentLang: Language = 'en';
 
-  constructor(public translationService: TranslationService) {
+
+  constructor(public translationService: TranslationService, private router: Router) {
     this.translationService.currentLang$.subscribe(lang => {
       this.currentLang = lang;
     });
+  }
+
+  isOnLegalNoticePage(): boolean {
+    return this.router.url.startsWith('/legal-notice');
   }
 
   setLanguage(lang: Language) {
